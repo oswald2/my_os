3aa6863badbb7a84d15af783439cbd34b7969dbb
Michael Oswald <michael.oswald@onikudaki.net>
2024-10-05 20:40:41 +0200

    factored out serial

--- src/boot.s
+++ src/boot.s
@@ -8,5 +8,11 @@ _start:
     mov x8, {}       // Move size of STACK into x8
     add x7, x7, x8   // Adjust stack pointer to end of memory region
     mov sp, x7       // Now set the stack pointer
+
+    # Enable FP in register
+    mrs x7, cpacr_el1
+    orr x7, x7, #(3 << 20)
+    msr cpacr_el1, x7
+
     # Call into main
     bl main
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,10 @@
 #![no_main]
 use core::{arch::global_asm, panic::PanicInfo};
 
+use serial::serial_puts;
+
+mod serial;
+
 #[no_mangle]
 #[link_section = ".stack"]
 static mut STACK: [u8; STACK_SIZE] = [0; STACK_SIZE];
@@ -12,22 +16,8 @@ global_asm!(include_str!("boot.s"), sym STACK, const STACK_SIZE);
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    serial_puts("PANIC !!!");
     loop {}
-}
-
-#[inline]
-fn serial_putchar(c: char) {
-    let uart_addr = 0x0900_0000 as *mut u8;
-
-    unsafe {
-        *uart_addr = c as u8;
-    }
-}
-
-fn serial_puts(s: &str) {
-    for c in s.chars() {
-        serial_putchar(c);
-    }
 }
 
 #[no_mangle]
